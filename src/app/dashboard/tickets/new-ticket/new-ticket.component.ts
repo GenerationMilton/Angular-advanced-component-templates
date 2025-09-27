1ff76bf144b8d35b8bac22f12b77a9757ce62ade
Milton Munoz <livemilton@gmail.com>
2025-09-26 22:02:48 -0500

    angular deep-dive :sparkles: Using The viewChild Signal Function

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -13,11 +13,13 @@ import { tick } from '@angular/core/testing';
 })
 export class NewTicketComponent {
 
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  //@ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
   onSubmit(title: string, ticketText: string) {
     console.log(title);
     console.log(ticketText);
-    this.form?.nativeElement.reset();
+
+    this.form().nativeElement.reset();
   }
 }
