e7b42a245ce62d990ed1586cbefb4ed27b10f4cf
Milton Munoz <livemilton@gmail.com>
2025-09-28 10:25:05 -0500

    angular deep-dive :sparkles: A Closer Look ath Decorator-based Queries & Lifecycle Hooks

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  viewChild,
+  ViewChild,
+} from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -11,15 +18,24 @@ import { tick } from '@angular/core/testing';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements AfterViewInit, OnInit {
+  @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  //private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
-  //@ViewChild('form') private form?: ElementRef<HTMLFormElement>;
-  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  ngOnInit(): void {
+    console.log('ONINIT');
+    console.log(this.form?.nativeElement);
+  }
+
+  ngAfterViewInit(): void {
+    console.log('AFTER VIEW INIT');
+    console.log(this.form?.nativeElement);
+  }
 
   onSubmit(title: string, ticketText: string) {
     console.log(title);
     console.log(ticketText);
 
-    this.form().nativeElement.reset();
+    this.form?.nativeElement.reset();
   }
 }
--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,5 @@
 import {
+  AfterContentInit,
   Component,
   contentChild,
   ContentChild,
@@ -22,7 +23,8 @@ import {
     '(click)': 'onClick()',
   },
 })
-export class ControlComponent {
+export class ControlComponent implements AfterContentInit {
+  
   // @HostBinding('class') className = 'control';
   // @HostListener('click') onClick() {
   //   console.log('Clicked!');
@@ -34,6 +36,10 @@ export class ControlComponent {
   //   HTMLInputElement | HTMLTextAreaElement
   // >;
   private control = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
+
+  ngAfterContentInit(): void {
+    
+  }
 
   onClick() {
     console.log('Clicked!');
