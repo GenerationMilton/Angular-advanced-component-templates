537007bbe765b616bcc390a25173f62642c81ebf
Milton Munoz <livemilton@gmail.com>
2025-09-28 10:38:56 -0500

    angular deep-dive :sparkles: Making Sense of Signal Effects

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnDestroy, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -9,11 +9,14 @@ import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core'
 })
 export class ServerStatusComponent implements OnInit {
 
-  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  // currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  currentStatus = signal<'online' | 'offline' | 'unknown' >('offline');
   private destroyRef = inject(DestroyRef);
 
   constructor(){
-    
+    effect(()=>{
+      console.log(this.currentStatus());
+    })
   }
   
 
@@ -23,11 +26,11 @@ export class ServerStatusComponent implements OnInit {
       const rnd = Math.random(); // 0 - 0.9999999999
 
       if(rnd < 0.5){
-        this.currentStatus = 'online';
+        this.currentStatus.set('online');
       } else if (rnd < 0.9){
-        this.currentStatus = 'offline';
+        this.currentStatus.set('offline');
       }else {
-        this.currentStatus = 'unknown';
+        this.currentStatus.set('unknown');
       }
     }, 5000);
 
